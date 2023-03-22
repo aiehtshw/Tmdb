a469d51981513e4bf7537d7f01e2413b1ddb2043
Nejdet Dalaslan <nejdet.dalaslan@hayriya.com>
2023-03-22 13:27:23 +0300

    Call movieCards unknown amount

--- src/screens/main/home.tsx
+++ src/screens/main/home.tsx
@@ -1,18 +1,53 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { COLORS } from "../../utils/colors";
 import MovieCard from "../../components/movieCard";
 import { Devices } from "../../config";
 import Background from "../../components/background";
 
 const HomeScreen = ({navigation}) => {
+  const Movies = [
+    {
+      uri:'https://pbs.twimg.com/profile_images/486929358120964097/gNLINY67_400x400.png',
+      movie_title:'Hello',
+      brief_overview:'asdafkmdkllfddlşflşsflşslşfdslşşfldkdfkmdfşlsşdglsşlgsgşdslgksdş'
+    },
+    {
+      uri:'https://pbs.twimg.com/profile_images/486929358120964097/gNLINY67_400x400.png',
+      movie_title:'Hello',
+      brief_overview:'asdafkmdkllfddlşflşsflşslşfdslşşfldkdfkmdfşlsşdglsşlgsgşdslgksdş'
+    },
+    {
+      uri:'https://pbs.twimg.com/profile_images/486929358120964097/gNLINY67_400x400.png',
+      movie_title:'Hello',
+      brief_overview:'asdafkmdkllfddlşflşsflşslşfdslşşfldkdfkmdfşlsşdglsşlgsgşdslgksdş'
+    },
+    {
+      uri:'https://pbs.twimg.com/profile_images/486929358120964097/gNLINY67_400x400.png',
+      movie_title:'Hello',
+      brief_overview:'asdafkmdkllfddlşflşsflşslşfdslşşfldkdfkmdfşlsşdglsşlgsgşdslgksdş'
+    },
+    {
+      uri:'https://pbs.twimg.com/profile_images/486929358120964097/gNLINY67_400x400.png',
+      movie_title:'Hello',
+      brief_overview:'asdafkmdkllfddlşflşsflşslşfdslşşfldkdfkmdfşlsşdglsşlgsgşdslgksdş'
+    },
+
+  ]
   return(
     <Background>
-      <MovieCard
-        uri={'https://pbs.twimg.com/profile_images/486929358120964097/gNLINY67_400x400.png'}
-        movie_title={'Hello'}
-        brief_overview={'asdafkmdkllfddlşflşsflşslşfdslşşfldkdfkmdfşlsşdglsşlgsgşdslgksdş'}
+      <FlatList
+        data={Movies}
+        keyExtractor={(item, index) => index}
+        renderItem={({ item }) => (
+          <MovieCard
+            uri={item.uri}
+            movie_title={item.movie_title}
+            brief_overview={item.brief_overview}
+          />
+        )}
       />
+
     </Background>
   );
 }
